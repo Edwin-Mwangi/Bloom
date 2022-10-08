375ea9048b3ed8fe31e8f46d96b34e36eb76faf9
Edwin Mwangi <edwinmwangi138@gmail.com>
2022-10-08 16:09:22 +0300

    tailwind config file to add custom styling

---
+++ tailwind.config.js
@@ -0,0 +1,42 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}",
+            './dist/index.html'
+          ],
+  theme: {
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'white': '#ffffff',
+        'base-blue': {
+          100: '#6abaf7',
+          200: '#53b4ff',
+          300: '#2279bd',
+          400: '#1a639b',
+          500: '#0019a7',
+        },
+      },
+      fontSize: {
+        normal: '.95rem',
+        larger: '1.05rem',
+      },
+      width: {
+        nav: '300px',
+        16.5: '75px',
+        custom: '46%',
+        0.75: '75px',
+      },
+      height: {
+        16.5: '75px',
+      },
+      margin: {
+        96.5: '400px'
+      }
+      
+    },
+
+  },
+  plugins: [],
+}
+

15f76c97cbd7a376e038310a0d5b304da4b98618
Edwin-Mwangi <edwinmwangi138@gmail.com>
2023-03-22 00:12:58 +0300

    gallery click behaviour in js

--- dist/script.js
+++ dist/script.js
@@ -1,7 +1,23 @@
 const imgOptions = document.querySelectorAll('div.header span');
+const gallery = document.querySelectorAll( 'div.album .img')
 
 imgOptions.forEach(option =>{
     option.addEventListener('click', ()=>{
-        console.log(`${option} clicked`)
+        if(option.innerHTML == 'All'){
+            gallery.forEach(image =>{
+                image.classList.remove('active', 'hidden')
+            })
+        }
+        if(option.innerHTML == 'Pretty'){
+            gallery.forEach(image =>{
+                image.classList.add('hidden')
+                if(image.classList.contains('five')||image.classList.contains('four')){
+                    image.classList.add('active')
+                    image.classList.remove('hidden')
+                }
+            })
+        }
+    
+
     })
 })

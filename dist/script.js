cde2b3201e15c80c5ea9b473f61ecadf9eeeddb8
Edwin-Mwangi <edwinmwangi138@gmail.com>
2023-03-26 19:58:20 +0300

    active class on nav btns during scroll

--- dist/script.js
+++ dist/script.js
@@ -1,13 +1,46 @@
 //nav options direction
-const navButtons = document.querySelectorAll('#menu li');
+const navButtons = document.querySelectorAll('#menu li a');
+const navLists = document.querySelectorAll('#menu li');
 
 navButtons.forEach(button =>{
     button.addEventListener('click', ()=>{
-        if(option.innerHTML == 'Intro'){
-            //scroll to start page
-        }
+        navLists.forEach(list =>{
+            if(list.classList.contains('active')){
+                list.classList.remove('active')
+            }
+        })
+        button.parentElement.classList.add('active')
     })
 } )
+//activating active when certain point is reached
+const sections = document.querySelectorAll('section');
+console.log(sections);
+window.addEventListener('scroll', checkSections);
+
+checkSections();
+
+function checkSections(){
+    const triggerBottom = window.innerHeight / 5 * 2;
+    for(i=0; i<sections.length; i++){
+        const sectionTop = sections[i].getBoundingClientRect().top
+        // console.log('triggerBtm:',triggerBottom,'section:', sectionTop)
+
+        if(sectionTop < triggerBottom){
+            navLists.forEach(list =>{
+                if(list.classList.contains('active')){
+                    list.classList.remove('active')
+                }
+            })
+            if(!navLists[i].classList.contains('active')){
+                navLists[i].classList.add('active')
+            }
+        }else{
+            navLists[i].classList.remove('active')
+        }
+        
+    }
+}
+
 
 //scroll behavior
 //tbd
@@ -20,13 +53,19 @@ imgOptions.forEach(option =>{
     option.addEventListener('click', ()=>{
         if(option.innerHTML == 'All'){
             gallery.forEach(image =>{
+                image.classList.add('hide')
                 if(image.classList.contains('active')){
-                    image.classList.add('hide')
                     setTimeout(()=>{
                         image.classList.add('hidden')        
                         image.classList.remove('active')                     
                     }, 400)                   
-                }
+                }//else{
+                //     //rest of images were displaying 1st so had to tweak(bruteforce)
+                //     image.classList.toggle('hide')
+                //     setTimeout(()=>{
+                //         image.classList.add('hide')                                
+                //     }, 400)
+                // }
                 setTimeout(()=>{
                     image.classList.remove('hidden','hide')                                
                 }, 400) 
@@ -41,6 +80,7 @@ imgOptions.forEach(option =>{
                         image.classList.add('active')
                         image.classList.remove('hidden','hide')
                     }
+                    image.classList.remove('hide')
                 }, 400)
             })
         }
